09e60b285726eba361769677378b7cb366103359
Devin Parkinson <dparky13@icloud.com>
2018-06-18 21:41:33 -0600

    fixing service worker issue

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Header } from 'semantic-ui-react';
+
 
 class About extends React.Component {
   render(){
     return (
       <div>
-      This is what I'm talking about...
+        <Header>Main Page w/ Bullets</Header>
+        <ul>Local</ul>
+        <ul>Responsive</ul>
+        <ul>Regenerative</ul>
+        <p>Mainly for building top soil. Makes them more enviromentally friendly. improve local food quality. reduce carbon footprint.</p>
+        <p>About page. Will receive what the about things are later.</p>
+        <p>Three step process on main page. Accordian style showing steps to complete process. Simple easy layout. FAQ at bottom. Main Logo is home link.</p>
+        <p>Contact Us</p>
+        <p>See if you are in the coverage area in contact page.</p>
       </div>
     )
   }
 }
 
-export default About
+export default About;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Accordian } from 'semantic-ui-react';
 import styled from 'styled-components';
 import { Carousel } from 'react-responsive-carousel';
 import './carousel.css';
-import './main.css';
 import Compost1 from '../images/compost1.jpg';
 import Compost2 from '../images/compost2.jpg';
 import Compost3 from '../images/compost3.jpeg';
@@ -22,10 +21,18 @@ const AppContainer = styled.div`
   color: #ffffff;
 `
 const FullApp = styled.div`
-  height: 100vh;
-  width: 100vw;
-  background-size: cover;
+  height: 100%;
+  width: 100%;
+  background-repeat: no-repeat;
+  background-size: contain;
   background-image: url("https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3380034.jpg");
+`
+
+const Carousel1 = styled(Carousel)`
+  height: 100vh !important;
+  width: 120vw !important;
+  display: flex !important;
+  justify-content: center !important;
 `
 
 class Home extends Component {
@@ -33,7 +40,8 @@ class Home extends Component {
     return (
       <FullApp>
         <AppContainer>
-          <Carousel autoPlay infiniteLoop width="fluid" showArrows={true}>
+          <Header as='h1' style={{fontFamily: 'Times New Roman', color: "#ffffff"}}>Bucket To Beak Curbside Composting</Header>
+          <Carousel1 autoPlay infiniteLoop width="fluid" showArrows={true}>
               <div>
                   <img src={Compost1} alt='Pic1' />
                   <p className="legend">Bilge water yardarm chase Yellow Jack splice the main brace bounty skysail lateen sail lanyard Sea Legs. Argggggh</p>
@@ -50,21 +58,12 @@ class Home extends Component {
                   <img src={Compost4} alt='Pic4' />
                   <p className="legend">Measured fer yer chains Yellow Jack spyglass Jack Tar no prey, no pay yardarm spirits sutler scuppers six pounders.</p>
               </div>
-        </Carousel>
-          <Header as='h1' style={{fontFamily: 'Times New Roman'}}>Bucket To Beak Curbside Composting</Header>
-          <Header>Main Page w/ Bullets</Header>
-          <ul>Local</ul>
-          <ul>Responsive</ul>
-          <ul>Regenerative</ul>
-          <p>Mainly for building top soil. Makes them more enviromentally friendly. improve local food quality. reduce carbon footprint.</p>
-          <p>About page. Will receive what the about things are later.</p>
-          <p>Three step process on main page. Accordian style showing steps to complete process. Simple easy layout. FAQ at bottom. Main Logo is home link.</p>
-          <p>Contact Us</p>
-          <p>See if you are in the coverage area in contact page.</p>
+        </Carousel1>
         </AppContainer>
       </FullApp>
     );
   }
 }
+
 
 export default Home;
